c05b101f547c96e1c9b807e99e559f65e4e79ae5
bobur <b.khaldarov@aues.kz>
2025-02-20 13:52:24 +0500

    add new changes and card-deleted batton fn()-del

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-const newPages=[5,4,3,2,1];
+const newPages = [5,4,3,2,1];
 
 const aboutCompanyFn=(text:string)=>text;
 const aboutCompany=aboutCompanyFn('О компании');
 
-const menuItems =['Каталог','Стройматериалы','Инструменты','Электрика','Интерьер и одежда']
+const menuItems = ['Каталог','Стройматериалы','Инструменты','Электрика','Интерьер и одежда']
 
 const UpperCaseMenuItems=menuItems.map(
   (items:string)=>{
@@ -29,22 +29,22 @@ export class AppComponent {
   isShowCatalog=false;
   isShowImg=false;
 
-  readonly header='Главная';
-  readonly header2='О компании';
-  readonly header3='Каталог';
+  readonly header = 'Главная';
+  readonly header2 = 'О компании';
+  readonly header3 = 'Каталог';
 
-  readonly header2item1=UpperCaseMenuItems[0];
-  readonly aboutCompany=aboutCompany;
-  menuItems=UpperCaseMenuItems;
-  readonly newPages=newPages;
+  readonly header2item1 = UpperCaseMenuItems[0];
+  readonly aboutCompany = aboutCompany;
+  menuItems = UpperCaseMenuItems;
+  readonly newPages = newPages;
 
   isUpperCase=true;
 
   changeMenuText(){
     this.menuItems = UpperCaseMenuItems.map(
-      item=>this.isUpperCase ? item.toLowerCase() : item.toUpperCase()
+      item => this.isUpperCase ? item.toLowerCase() : item.toUpperCase()
     )
-    this.isUpperCase=!this.isUpperCase
+    this.isUpperCase =! this.isUpperCase
   }
 
 }
